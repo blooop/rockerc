069188d3ef88aa3e723a7a5aa9c74b8946fae563
Austin Gregg-Smith <blooop@gmail.com>
2025-07-28 19:29:18 +0100

    update tests

--- test/workflows/test_workflow_1_clone_and_work.sh
+++ test/workflows/test_workflow_1_clone_and_work.sh
@@ -3,5 +3,11 @@ set -e
 cd /tmp
 rm -rf /tmp/renv
 
+#rockerc not set up in this repo
 echo "Running: renv osrf/rocker"
-renv osrf/rocker echo "finished work!"
+renv osrf/rocker pwd; git status
+
+#rockerc is set up in this repo
+echo "Running: renv blooop/manifest_rocker@renv_test"
+renv blooop/manifest_rocker@renv_test pwd; git status
+
