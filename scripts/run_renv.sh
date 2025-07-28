eedf60a6cac13a7b779c7f92c78078fbaac2551c
Austin Gregg-Smith <blooop@gmail.com>
2025-07-28 12:02:04 +0100

    fix run renv

--- scripts/run_renv.sh
+++ scripts/run_renv.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-# Remove the renv directory and run renv with the specified argument
-set -oux pipefail
-    # Remove any existing renv directory in /tmp
-    cd /tmp || exit 1
-    rm -rf renv
-    # Copy the project to /tmp/rockerc_test and cd into it
-    cp -r "$(dirname "$0")/.." /tmp/rockerc_test
-    cd /tmp/rockerc_test || exit 1
-    # Run renv with the provided argument
-    renv osrf/rocker
+set -euxo pipefail
+cd /tmp || exit 1
+rm -rf renv
+renv osrf/rocker
