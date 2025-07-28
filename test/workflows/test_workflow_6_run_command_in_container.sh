2e3e15f43c7df5931aa74b7a945d965826320adc
Austin Gregg-Smith <blooop@gmail.com>
2025-07-28 19:32:13 +0100

    clean up tests

--- test/workflows/test_workflow_6_run_command_in_container.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd /tmp
-rm -rf /tmp/renv
-
-echo "Running: renv osrf/rocker git status (with timeout)"
-# renv osrf/rocker git status
-renv osrf/rocker 
--- test/workflows/test_workflow_7_force_rebuild_cache.sh
+++ test/workflows/test_workflow_7_force_rebuild_cache.sh
@@ -5,3 +5,6 @@ rm -rf /tmp/renv
 
 echo "Running: renv blooop/manifest_rocker@main -f"
 renv blooop/manifest_rocker@main -f
+
+echo "Running: renv blooop/manifest_rocker@main --nocache"
+renv blooop/manifest_rocker@main --nocache
