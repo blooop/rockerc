25791d9f35a8488c58ec4c632a2b02c6abe3904f
Austin Gregg-Smith <blooop@gmail.com>
2025-07-29 09:47:37 +0100

    update swtich branch test

--- test/workflows/test_workflow_2_switch_branch.sh
+++ test/workflows/test_workflow_2_switch_branch.sh
@@ -4,11 +4,13 @@ cd /tmp
 rm -rf /tmp/renv
 
 echo "Running: renv osrf/rocker echo 'persistent message' > tmp.txt"
-renv osrf/rocker echo "persistent message" > tmp.txt
+renv osrf/rocker ls
 
-echo "Running: renv osrf/rocker@new_branch touch tmp2.txt"
-renv osrf/rocker@new_branch touch tmp2.txt
-renv osrf/rocker echo "new_branch" > tmp.txt
+echo "Running: renv osrf/rocker echo 'persistent message' > tmp.txt"
+renv osrf/rocker touch persistent.txt
 
-echo "Running: renv osrf/rocker cat tmp.txt"
-renv osrf/rocker echo "contents of tmp.txt: " && cat tmp.txt
+echo "Running: renv osrf/rocker echo 'persistent message' > tmp.txt"
+renv osrf/rocker ls
+
+# echo "Running: renv osrf/rocker cat tmp.txt"
+# renv osrf/rocker echo "contents of tmp.txt: " && cat tmp.txt
