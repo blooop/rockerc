e3234c877c3869adc28acf362ae9c62b7dc16bf8
Austin Gregg-Smith <blooop@gmail.com>
2025-10-04 15:48:41 +0100

    remove all containeres

--- test/workflows/test_workflow_0_fresh_container.sh
+++ test/workflows/test_workflow_0_fresh_container.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 rm -rf ~/renv
+# Remove all containers with test_renv in the name
+docker ps -a --format '{{.Names}}' | grep 'test_renv' | xargs -r docker rm -f
 cd /tmp
 
 renv blooop/test_renv git status
