755dce7d50e5f05f07616b142b4852154ea287b6
Austin Gregg-Smith <blooop@gmail.com>
2025-10-07 23:21:09 +0100

    update subfolder test

--- test/workflows/test_workflow_8_subfolder.sh
+++ test/workflows/test_workflow_8_subfolder.sh
@@ -4,14 +4,13 @@ set -euo pipefail
 RENV_DIR="${RENV_DIR:-$HOME/renv}"
 rm -rf "${RENV_DIR}"
 
-#should enter on main repo @ branch nf3
+#should enter on main repo @ branch nf3 and contain the folder "folder1"
 renv blooop/test_renv@nf3 ls
-
 
 #should enter on main repo @ branch nf3 in subfolder folder1 and contain the folder "folder2"
 renv blooop/test_renv@nf3#folder1 ls
 
-#should enter on main repo @ branch nf3 in subfolder folder1/folder and contain the file "example.txt"
-renv blooop/test_renv@nf3#folder1 ls
+#should enter on main repo @ branch nf3 in subfolder folder1/folder2 and contain the file "example.txt"
+renv blooop/test_renv@nf3#folder1/folder2 ls
 
 echo "✓ Subfolder workflow validated"
