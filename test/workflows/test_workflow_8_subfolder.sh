1e6c530cb8498353ffbf08456679140ed6be5ade
Austin Gregg-Smith <blooop@gmail.com>
2025-10-08 20:41:22 +0100

    add more folder tests

--- test/workflows/test_workflow_8_subfolder.sh
+++ test/workflows/test_workflow_8_subfolder.sh
@@ -4,13 +4,19 @@ set -euo pipefail
 RENV_DIR="${RENV_DIR:-$HOME/renv}"
 rm -rf "${RENV_DIR}"
 
-#should enter on main repo @ branch nf3 and contain the folder "folder1"
-renv blooop/test_renv@nf3 ls
+#should enter on main repo @ branch test_branch1 and contain the folder "folder1"
+renv blooop/test_renv@test_branch1 ls
 
-#should enter on main repo @ branch nf3 in subfolder folder1 and contain the folder "folder2"
-renv blooop/test_renv@nf3#folder1 ls
+#should enter on main repo @ branch test_branch1 in subfolder folder1 and contain the folder "folder2"
+renv blooop/test_renv@test_branch1#folder1 ls
 
-#should enter on main repo @ branch nf3 in subfolder folder1/folder2 and contain the file "example.txt"
-renv blooop/test_renv@nf3#folder1/folder2 ls
+#should enter on main repo @ branch test_branch1 in subfolder folder1/folder2 and contain the file "example.txt"
+renv blooop/test_renv@test_branch1#folder1/folder2 ls
+
+#should enter on main repo @ branch test_branch1 in subfolder folder1/folder2 and contain the file "example.txt"
+renv blooop/test_renv@test_branch1#folder1/folder2 ls
+
+#should enter on main repo @ branch test_branch2 in subfolder folder1/folder2 and contain the file "example.txt"
+renv blooop/test_renv@test_branch1#folder1/folder2 ls
 
 echo "✓ Subfolder workflow validated"
